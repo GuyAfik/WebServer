e44a62e5fa37afac7916e5d9ae853b41fa9e0353
Guy Afik <53861351+GuyAfik@users.noreply.github.com>
2022-06-18 13:18:32 +0300

    add user entity (#5)

---
+++ server/internal/entities/user.go
@@ -0,0 +1,11 @@
+// Package entites defines main entities for business logic (services), data base mapping and
+// HTTP response objects if suitable. Each logic group entities in own file.
+package entities
+
+
+// User -.
+type User struct {
+	UserName string `json:"username" example:"username123"`
+	Password string `json:"password" example:"abdjf841235"`
+	Email string `json:"email" example:"test@gmail.com"`
+}

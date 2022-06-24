c42e7748f1c6773a549eab7bfbfdb24049951c98
GuyAfik <guyafik11@gmail.com>
2022-06-24 16:34:51 +0300

    fixes

--- server/internal/entities/user.go
+++ server/internal/entities/user.go
@@ -2,9 +2,9 @@
 // HTTP response objects if suitable. Each logic group entities in own file.
 package entities
 
-// User -.
-type User struct {
-	UserName string `json:"username" example:"username123"`
-	Password string `json:"password" example:"abdjf841235"`
-	Email    string `json:"email" example:"test@gmail.com"`
+// UserEntity -.
+type UserEntity struct {
+	Username string `json:"username" example:"username123" binding:"required"`
+	Password string `json:"password" example:"abdjf841235" binding:"required"`
+	Email    string `json:"email" example:"test@gmail.com" binding:"required"`
 }
--- server/internal/usecases/user/db/user_db.go
+++ server/internal/usecases/user/db/user_db.go
@@ -8,17 +8,16 @@ import (
 )
 
 type UserDBService struct {
-
 }
 
 func New() *UserDBService {
 	return &UserDBService{}
 }
 
-func (uDBs *UserDBService) Get(ctx context.Context, userID string) (*entities.User, *http_utils.ApiErrorResponse) {
-	return &entities.User{UserName: "1234", Password: "1234", Email: "test@gmail.com"}, nil
+func (uDBs *UserDBService) Get(ctx context.Context, userID string) (*entities.UserEntity, *http_utils.ErrorResponse) {
+	return &entities.UserEntity{Username: "1234", Password: "1234", Email: "test@gmail.com"}, nil
 }
 
-func (uDBs *UserDBService) Store(ctx context.Context, userEntity *entities.User) *http_utils.ApiErrorResponse {
+func (uDBs *UserDBService) Store(ctx context.Context, userEntity *entities.UserEntity) *http_utils.ErrorResponse {
 	return nil
 }
